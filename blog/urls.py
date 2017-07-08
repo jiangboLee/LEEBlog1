905c0fcd77754859684d6f2cdb296a86ee386004
李江波 <584556825@qq.com>
2017-07-09 01:30:54 +0800

    睡觉了

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blog'
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
     url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
     url(r'^search/$', views.search, name='search'),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
