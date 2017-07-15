3e60493c418fed53ec18ad90a79edfd5006a5df2
李江波 <584556825@qq.com>
2017-07-15 11:06:45 +0800

    分页

--- blog/views.py
+++ blog/views.py
@@ -28,6 +28,65 @@ class IndexView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
     paginate_by = 1
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginator = context.get('is_paginated')
+        # 调用自己写的 pagination_data 方法获得显示分页导航条需要的数据
+        paginator_date = self.pagination_data(paginator, page, is_paginator)
+        # 将分页导航条的模板变量更新到 context 中
+        context.update(paginator_date)
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        # 获得用户当前请求的页码号
+        page_number = page.number
+        # 获得分页后的总页数
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            # 这里只获取了当前页码后连续两个页码，你可以更改这个数字以获取更多页码。
+            right = page_range[page_number: page_number + 2];
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number -1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number -1]
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        context = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+        return  context
 
 
 
