1d6c2e5747a8f3f4bc4b60ee9d8bd1a0323b3128
李江波 <584556825@qq.com>
2017-07-25 23:49:16 +0800

    修改

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
     url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
     url(r'^tag/(?P<pk>[0-9]+)/', views.tag, name='tag'),
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Tag
 import markdown
+from markdown.extensions import toc
 from comments.forms import CommentForm
 from markdown.extensions.toc import TocExtension
 from django.utils.text import slugify
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 # def index(request):
@@ -93,25 +94,57 @@ class IndexView(ListView):
 
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    # 阅读量+1
-    post.increase_views()
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        TocExtension(slugify=slugify)
-    ])
-    post.body = md.convert(post.body)
-    form = CommentForm()
-    comment_list = post.comment_set.all()
-    context = {
-        'post': post,
-        'form': form,
-        'comment_list': comment_list,
-        'toc': md.toc,
-    }
-    return render(request, 'blog/detail.html', context=context)
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     # 阅读量+1
+#     post.increase_views()
+#     md = markdown.Markdown(extensions=[
+#         'markdown.extensions.extra',
+#         'markdown.extensions.codehilite',
+#         'markdown.extensions.toc',
+#         # TocExtension(slugify=slugify)
+#     ])
+#     post.body = md.convert(post.body)
+#     form = CommentForm()
+#     comment_list = post.comment_set.all()
+#     context = {
+#         'post': post,
+#         'form': form,
+#         'comment_list': comment_list,
+#         'toc': md.toc,
+#     }
+#     return render(request, 'blog/detail.html', context=context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+          'markdown.extensions.extra',
+          'markdown.extensions.codehilite',
+          TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+        })
+        return context
 
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
